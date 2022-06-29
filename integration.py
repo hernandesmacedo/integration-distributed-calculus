11bf67ecb357b2faa02fe5ef60afbde873d61948
hernandesmacedo <hcarneiromacedo@gmail.com>
2022-06-29 10:28:07 -0300

    f function defined

---
+++ integration.py
@@ -0,0 +1,10 @@
+def f(x: float):
+    """Defines the function you want to calculate the integration.
+
+    Args:
+        x (float): x value
+
+    Returns:
+        float: The result of 5*x^3 + 3*x^2 + 4*x + 20
+    """
+    return 5 * pow(x, 3) + 3 * pow (x, 2) + 4 * x + 20
